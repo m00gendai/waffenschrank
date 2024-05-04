0f45aaed22e29d508a424ed89081f7f7274b299d
m00gendai <mrweber@gmx.ch>
2024-05-04 09:33:28 +0200

    Added PaperProvider Added NewGun component

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import GunCollection from "./components/GunCollection"
+import NewGun from "./components/NewGun"
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      
+      <NewGun />
+    </PaperProvider>
   );
 }
 
