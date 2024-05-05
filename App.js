7bec27003a60747e108cc1921c47cd5528e892de
m00gendai <mrweber@gmx.ch>
2024-05-05 09:24:42 +0200

    Rudimentary display of SecureStore data

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 import GunCollection from "./components/GunCollection"
 import NewGun from "./components/NewGun"
+import * as SecureStore from "expo-secure-store"
+
+function getGuns(){
+  const guns = SecureStore.getItem("gunx")
+  return guns ? JSON.parse(guns) : {}
+}
 
 export default function App() {
 
+  const guns = getGuns()
   return (
     <PaperProvider>
-      
+      <View style={styles.container}>
+      {
+        guns.map(gun =>{
+          return <View key={gun.id}>
+            {Object.entries(gun).map(a =>{
+              return <Text key={`text_${a[0]}_${gun.id}`}>{`${a[0]}: ${a[1]}`}</Text>
+            })}
+          </View>
+        })
+      }
+    </View>
       <NewGun />
     </PaperProvider>
   );
