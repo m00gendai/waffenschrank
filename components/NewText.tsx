0d28b7f8ee6c54298d8aa1a935c9a1284dc0e1f0
m00gendai <mrweber@gmx.ch>
2024-05-09 13:30:40 +0200

    Added types

--- components/NewText.tsx
+++ components/NewText.tsx
@@ -1,12 +1,18 @@
 import { TextInput } from 'react-native-paper';
 import { useState } from 'react';
+import { GunType } from '../interfaces';
 
+interface Props{
+    data: string
+    gunData: GunType
+    setGunData: React.Dispatch<React.SetStateAction<GunType>>
+}
 
-export default function NewText({data, gunData, setGunData}){
+export default function NewText({data, gunData, setGunData}: Props){
 
-    const [input, setInput] = useState(gunData[data])
+    const [input, setInput] = useState<string>(null)
 
-    function updateGunData(input){
+    function updateGunData(input:string){
         setInput(input)
         setGunData({...gunData, [data]: input})
     }
