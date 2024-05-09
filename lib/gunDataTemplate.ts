2bc9693ae2519fc8b80aaa6a3e071dd7684e39eb
m00gendai <mrweber@gmx.ch>
2024-05-09 13:30:23 +0200

    Added types

---
+++ lib/gunDataTemplate.ts
@@ -0,0 +1,12 @@
+export const gunDataTemplate:string[] = [
+    "Hersteller",
+    "Modellbezeichnung",
+    "HerstellungsDatum",
+    "Waffentyp",
+    "Funktionsweise",
+    "Kaliber", 
+    "Seriennummer",
+    "Bewilligung",
+    "Erwerbsdatum", 
+    "Hauptfarbe",
+]
