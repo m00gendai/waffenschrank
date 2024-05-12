27cc5ea73a964fe5c2a7bd1a1a329e73c4c24cc1
m00gendai <mrweber@gmx.ch>
2024-05-12 23:16:51 +0200

    Multi language text snippets

---
+++ textTemplates.ts
@@ -0,0 +1,94 @@
+export const editGunTitle:{de:string, en:string, fr:string} = {
+    de: "Waffe bearbeiten",
+    en: "Edit Gun",
+    fr: "Editer le Gun",
+}
+
+export const newGunTitle:{de:string, en:string, fr:string} = {
+    de: "Neue Waffe",
+    en: "New Gun",
+    fr: "Nouveau Gun"
+}
+
+export const unsavedChangesAlert:{[key: string]: {de:string, en:string, fr:string}} = {
+    title: {
+        de: "Es hat nicht gespeicherte Änderungen",
+        en: "Unsaved changes",
+        fr: "Il y a des unsaved changes",
+    },
+    subtitle: {
+        de: "Wirklich zurück?",
+        en: "Continue?",
+        fr: "Continuer?",
+    },
+    yes: {
+        de: "Ja",
+        en: "Yes",
+        fr: "Oui",
+    },
+    no: {
+        de: "Nein",
+        en: "No",
+        fr: "Non"
+    }
+}
+
+export const imageDeleteAlert:{[key: string]: {de:string, en:string, fr:string}} = {
+    title: {
+        de: "Bild wirklich löschen?",
+        en: "Delete image?",
+        fr: "Deleter le image?",
+    },
+    yes: {
+        de: "Ja",
+        en: "Yes",
+        fr: "Oui",
+    },
+    no: {
+        de: "Nein",
+        en: "No",
+        fr: "Non"
+    }
+}
+
+export const validationFailedAlert:{[key: string]: {de:string, en:string, fr:string}} = {
+    title: {
+        de: "Validierung fehlgeschlagen",
+        en: "Validation failed",
+        fr: "Le validation a failer",
+    },
+    no: {
+        de: "OK",
+        en: "OK",
+        fr: "OK"
+    }
+}
+
+export const validationErros:{[key: string]: {de:string, en:string, fr:string}} = {
+    requiredFieldEmpty: {
+        de: "Feld darf nicht leer sein",
+        en: "Field can not be empty",
+        fr: "Le field ne can pas be empty",
+    },
+}
+
+export const toastMessages:{[key: string]: {de:string, en:string, fr:string}} = {
+    saved: {
+        de: "gespeichert",
+        en: "saved",
+        fr: "le saved"
+    },
+    changed: {
+        de: "geändert",
+        en: "changed",
+        fr: "le changed"
+    }
+}
+
+export const preferenceTitles:{[key:string]: {de: string, en: string, fr: string}} = {
+    language: {
+        de: "Sprache ändern",
+        en: "Change language",
+        fr: "Changer le language"
+    }
+}
