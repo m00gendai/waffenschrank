1004e3a38ea92af3dc9466fcfd073bf207bfa1e6
m00gendai <mrweber@gmx.ch>
2024-05-20 11:58:20 +0200

    New keys

--- configs.ts
+++ configs.ts
@@ -2,6 +2,7 @@ export const defaultGridGap:number = 10
 
 export const defaultViewPadding:number = 10
 
-export const KEY_DATABASE:string = "keyS"
-export const GUN_DATABASE:string = "gunS"
-export const PREFERENCES:string = "preS"
+export const KEY_DATABASE:string = "key0008"
+export const GUN_DATABASE:string = "gun0008"
+export const PREFERENCES:string = "pre0008"
+export const TAGS:string = "tag0008"
