23e91b44d7dcd4eb3ec4a4dcd0bf79e5b5596383
m00gendai <mrweber@gmx.ch>
2024-06-06 10:51:49 +0200

    New ammo data template added

---
+++ lib/ammoDataTemplate.ts
@@ -0,0 +1,57 @@
+export const ammoDataTemplate:{name:string, de:string, en:string, fr:string}[] = [
+    {
+            name: "manufacturer",
+            de: "Hersteller",
+            en: "Manufacturer",
+            fr: "Fabricant",
+    },
+    {   
+            name: "designation",
+            de: "Bezeichnung",
+            en: "Designation",
+            fr: "Désignation",
+    },
+    {
+            name: "originCountry",
+            de: "Ursprungsland",    
+            en: "Origin Country",
+            fr: "Pays d'origine",
+    },
+    {
+            name: "caliber",
+            de: "Kaliber",
+            en: "Caliber",
+            fr: "Calibre",
+    },
+    {
+            name: "headstamp",
+            de: "Hülsenboden",
+            en: "Headstamp",
+            fr: "Cachet",
+    },
+    {
+        name: "currentStock",
+        de: "Aktuelle Menge",
+        en: "Current stock",
+        fr: "Montant actuel",
+},
+{
+    name: "lastTopupAt",
+    de: "Zuletzt aufgestockt",
+    en: "Last topped up",
+    fr: "Dernièrement augmenté",
+},
+{
+    name: "criticalStock",
+    de: "Kritische Menge",
+    en: "Critical stock",
+    fr: "Quantité critique",
+}
+]
+
+export const ammoRemarks:{name:string, de:string, en:string, fr:string} = {
+    name: "remarks",
+    de: "Bemerkungen",
+    en: "Remarks",
+    fr: "Remarques",
+}
