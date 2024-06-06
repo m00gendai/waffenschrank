0350ecd1305f09db27b4785ce0f983dbb90cab58
m00gendai <mrweber@gmx.ch>
2024-06-06 10:58:39 +0200

    Amended for new ammo collection

--- stores/useTagStore.ts
+++ stores/useTagStore.ts
@@ -4,10 +4,16 @@ interface TagStore {
     tags: {label: string, status: boolean}[]
     setTags: (tag: {label: string, status: boolean}) => void
     overWriteTags: (tags:{label:string, status:boolean}[]) => void
+    ammo_tags: {label: string, status: boolean}[]
+    setAmmoTags: (ammo_tag: {label: string, status: boolean}) => void
+    overWriteAmmoTags: (ammo_tags:{label:string, status:boolean}[]) => void
   }
 
   export const useTagStore = create<TagStore>((set) => ({
     tags: [],
     setTags: (tag: {label: string, status: boolean}) => set((state) => ({tags: [...state.tags, tag]})),
-    overWriteTags: (tags: {label: string, status:boolean}[]) => set((state)=>({tags: tags}))
+    overWriteTags: (tags: {label: string, status:boolean}[]) => set((state)=>({tags: tags})),
+    ammo_tags: [],
+    setAmmoTags: (ammo_tag: {label: string, status: boolean}) => set((state) => ({ammo_tags: [...state.ammo_tags, ammo_tag]})),
+    overWriteAmmoTags: (ammo_tags: {label: string, status:boolean}[]) => set((state)=>({ammo_tags: ammo_tags}))
   }))
