5564f3927dd1ba5209e5599e90defedc577b48ac
m00gendai <mrweber@gmx.ch>
2024-06-06 10:52:32 +0200

    New databases for ammo

--- configs.ts
+++ configs.ts
@@ -4,10 +4,20 @@ export const defaultGridGap:number = 10
 
 export const defaultViewPadding:number = 10
 
+
+/* Gun Collection databases */
 export const KEY_DATABASE:string = "key0008"
 export const GUN_DATABASE:string = "gun0008"
-export const PREFERENCES:string = "pre0008"
 export const TAGS:string = "tag0008"
+
+/* Ammo Collection databases */
+export const A_KEY_DATABASE:string = "akey0008"
+export const AMMO_DATABASE:string = "ammo0008"
+export const A_TAGS:string = "atag0008"
+
+/* Preferences database is shared */
+export const PREFERENCES:string = "pre0008"
+
 
 export const dateLocales:SimpleTranslation = {
     de: "de-CH",
