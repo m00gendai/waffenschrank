eed1f412bd6cba848ef6ad174f64eff427d3a0e2
m00gendai <mrweber@gmx.ch>
2024-06-06 10:46:34 +0200

    New Ammo component

---
+++ components/Ammo.tsx
@@ -0,0 +1,168 @@
+import { StyleSheet, Text, View, Modal, ScrollView, Alert, TouchableNativeFeedback, TouchableOpacity } from 'react-native';
+import { Button, Appbar, Icon, Chip } from 'react-native-paper';
+import { ammoDataTemplate, ammoRemarks } from "../lib/ammoDataTemplate"
+import * as SecureStore from "expo-secure-store"
+import { useState} from "react"
+import EditGun from "./EditGun"
+import ImageViewer from "./ImageViewer"
+import { AMMO_DATABASE, A_KEY_DATABASE } from '../configs';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { usePreferenceStore } from '../stores/usePreferenceStore';
+import { useViewStore } from '../stores/useViewStore';
+import { useAmmoStore } from '../stores/useAmmoStore';
+import { ammoDeleteAlert } from '../lib/textTemplates';
+import { printSingleGun } from '../functions/printToPDF';
+
+
+export default function Ammo(){
+
+    const [lightBoxIndex, setLightBoxIndex] = useState<number>(0)
+
+    const { setSeeAmmoOpen, editAmmoOpen, setEditAmmoOpen, lightBoxOpen, setLightBoxOpen } = useViewStore()
+    const { language } = usePreferenceStore()
+    const { currentAmmo, setCurrentAmmo } = useAmmoStore()
+
+    const showModal = (index:number) => {
+        setLightBoxOpen()
+        setLightBoxIndex(index)
+    }
+
+    const styles = StyleSheet.create({
+        container: {
+            display: "flex",
+            flex: 1,
+            flexWrap: "wrap",
+            flexDirection: "column",
+            height: "100%",
+            width: "100%",
+            justifyContent: "center",
+            alignItems: "flex-start",
+            alignContent: "flex-start",
+            gap: 5,
+            padding: 5,
+        },
+        imageContainer: {
+            width: "100%",
+            aspectRatio: currentAmmo.images && currentAmmo.images.length == 1 ? "21/10" : "18/10",
+            flexDirection: "column",
+            flex: 1,
+            marginRight: 5
+        },
+        data: {
+            flex: 1,
+            height: "100%",
+            width: "100%",
+            marginTop: 10
+        },
+    })
+
+    async function deleteItem(ammo){
+        // Deletes ammo in gun database
+        await SecureStore.deleteItemAsync(`${AMMO_DATABASE}_${ammo.id}`)
+
+        // retrieves ammo ids from key database and removes the to be deleted id
+        const keys:string = await AsyncStorage.getItem(A_KEY_DATABASE)
+        const keyArray: string[] = JSON.parse(keys)
+        const newKeys: string[] = keyArray.filter(key => key != ammo.id)
+        AsyncStorage.setItem(A_KEY_DATABASE, JSON.stringify(newKeys))
+        setSeeAmmoOpen()
+    }
+    
+    function invokeAlert(ammo){
+        Alert.alert(`${ammo.designation} ${ammoDeleteAlert.title[language]}`, ammoDeleteAlert.subtitle[language], [
+            {
+                text: ammoDeleteAlert.yes[language],
+                onPress: () => deleteItem(ammo)
+            },
+            {
+                text: ammoDeleteAlert.no[language],
+                style: "cancel"
+            }
+        ])
+    }
+
+    return(
+        <View style={{width: "100%", height: "100%", backgroundColor: "white"}}>
+            
+            <Appbar style={{width: "100%"}}>
+                <Appbar.BackAction  onPress={() => setSeeAmmoOpen()} />
+                <Appbar.Content title={`${currentAmmo.designation} ${currentAmmo.manufacturer}`} />
+               {/* <Appbar.Action icon="printer" onPress={()=>printSingleGun(currentGun, language)} /> */}
+                <Appbar.Action icon="pencil" onPress={setEditAmmoOpen} />
+            </Appbar>
+        
+            <View style={styles.container}>   
+                <ScrollView style={{width: "100%"}}>
+                    <View style={{backgroundColor: "transparent"}}>
+                        <ScrollView horizontal style={{width:"100%", aspectRatio: "21/10", marginTop: 10, marginBottom: 10}}>
+                            {Array.from(Array(5).keys()).map((_, index) =>{
+                            
+                                if(currentAmmo.images && index <= currentAmmo.images.length-1){
+                                    return(
+                                        <TouchableNativeFeedback key={`slides_${index}`} onPress={()=>showModal(index)}>
+                                            <View style={styles.imageContainer} >
+                                            <ImageViewer isLightBox={false} selectedImage={currentAmmo.images[index]} /> 
+                                            </View>
+                                        </TouchableNativeFeedback>
+                                    )
+                                }      
+                                if(!currentAmmo.images || currentAmmo.images.length === 0){
+                                    return(
+                                        <TouchableNativeFeedback key={`slides_${index}`}>
+                                            <View style={styles.imageContainer} >
+                                            <ImageViewer isLightBox={false} selectedImage={null} /> 
+                                            </View>
+                                        </TouchableNativeFeedback>
+                                    )
+                                }                   
+                            })}
+                        </ScrollView>
+                    </View>
+                    <View style={styles.data}>
+                        <View style={{flex: 1, flexDirection: "row", flexWrap: "wrap", marginBottom: 10}}>
+                            {currentAmmo.tags?.map((tag, index) =>{
+                                return <View key={`${tag}_${index}`} style={{padding: 5}}><Chip >{tag}</Chip></View>
+                            })}
+                        </View>
+                        {ammoDataTemplate.map((item, index)=>{
+                            return(
+                                <View key={`${item.name}`} style={{flex: 1, flexDirection: "column"}} >
+                                    <Text style={{width: "100%", fontSize: 12,}}>{`${item[language]}:`}</Text>
+                                    <Text style={{width: "100%", fontSize: 18, marginBottom: 5, paddingBottom: 5, borderBottomColor: "black", borderBottomWidth: 0.2}}>{currentAmmo[item.name]}</Text>
+                                </View>
+                            )
+                        })}
+                        <View style={{flex: 1, flexDirection: "column"}} >
+                            <Text style={{width: "100%", fontSize: 12,}}>{ammoRemarks[language]}</Text>
+                            <Text style={{width: "100%", fontSize: 18, marginBottom: 5, paddingBottom: 5, borderBottomColor: "black", borderBottomWidth: 0.2}}>{currentAmmo.remarks}</Text>
+                        </View>
+                    </View>
+                    
+                    <Modal visible={editAmmoOpen}>
+                        <EditGun />
+                    </Modal>
+                    
+                    <Modal visible={lightBoxOpen} transparent>
+                        <View style={{width: "100%", height: "100%", padding: 0, flex: 1, flexDirection: "column", flexWrap: "wrap"}}>
+                            <View style={{width: "100%", flexDirection: "row", justifyContent:"flex-end", alignItems: "center", alignContent: "center", backgroundColor: "black", flex: 2}}>
+                                <View style={{backgroundColor: "black", padding: 0}}>
+                                    <TouchableOpacity onPress={setLightBoxOpen} style={{padding: 10}}>
+                                        <Icon source="close-circle-outline" size={25} color='white'/>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                          {lightBoxOpen ? <ImageViewer isLightBox={true} selectedImage={currentAmmo.images[lightBoxIndex]}/> : null}
+                        </View>
+                    </Modal>                    
+                    
+                    <View style={{width: "100%", display: "flex", flex: 1, flexDirection: "row", justifyContent:"center"}}>
+                        <Button mode="contained" style={{width: "20%", backgroundColor: "red", marginTop: 20}} onPress={()=>invokeAlert(currentAmmo)}>
+                            <Icon source="delete" color='white' size={20}/>
+                        </Button>
+                    </View>
+                </ScrollView>
+            </View>
+        </View>
+    )
+}
+
---
+++ components/NewAmmo.tsx
@@ -0,0 +1,262 @@
+import { StyleSheet, View, ScrollView, Alert } from 'react-native';
+import { Appbar, FAB, Snackbar } from 'react-native-paper';
+import * as ImagePicker from "expo-image-picker"
+import { useEffect, useState } from 'react';
+import * as SecureStore from "expo-secure-store"
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { ammoDataTemplate, ammoRemarks } from "../lib/ammoDataTemplate"
+import NewText from "./NewText"
+import "react-native-get-random-values"
+import { v4 as uuidv4 } from 'uuid';
+import ImageViewer from "./ImageViewer"
+import { AMMO_DATABASE, A_KEY_DATABASE } from '../configs';
+import { AmmoType } from '../interfaces';
+import { ammoDataValidation } from '../utils';
+import NewTextArea from './NewTextArea';
+import NewCheckboxArea from './NewCheckboxArea';
+import { newAmmoTitle, toastMessages, unsavedChangesAlert, validationFailedAlert } from '../lib/textTemplates';
+import { usePreferenceStore } from '../stores/usePreferenceStore';
+import { useViewStore } from '../stores/useViewStore';
+import NewChipArea from './NewChipArea';
+import { useAmmoStore } from '../stores/useAmmoStore';
+
+
+export default function newAmmo(){
+
+    const [selectedImage, setSelectedImage] = useState<string[]>(null)
+    const [granted, setGranted] = useState<boolean>(false)
+    const [ammoData, setAmmoData] = useState<AmmoType>(null)
+    const [visible, setVisible] = useState<boolean>(false);
+    const [snackbarText, setSnackbarText] = useState<string>("")
+    const [saveState, setSaveState] = useState<boolean>(false)
+
+    const { language } = usePreferenceStore()
+    const { setNewAmmoOpen, setSeeAmmoOpen } = useViewStore()
+    const { setCurrentAmmo } = useAmmoStore()
+
+    useEffect(()=>{
+        setSaveState(false)
+    },[ammoData])
+
+    function invokeAlert(){
+        Alert.alert(unsavedChangesAlert.title[language], unsavedChangesAlert.subtitle[language], [
+            {
+                text: unsavedChangesAlert.yes[language],
+                onPress: setNewAmmoOpen
+            },
+            {
+                text: unsavedChangesAlert.no[language],
+                style: "cancel"
+            }
+        ])
+    }
+
+  const onToggleSnackBar = () => setVisible(!visible);
+
+  const onDismissSnackBar = () => setVisible(false);
+
+    async function save(value:AmmoType) {
+        const validationResult:{field: string, error: string}[] = ammoDataValidation(value, language)
+        if(validationResult.length != 0){
+            Alert.alert(validationFailedAlert.title[language], `${validationResult.map(result => `${result.field}: ${result.error}`)}`, [
+                {
+                    text: validationFailedAlert.no[language],
+                    style: "cancel"
+                }
+            ])
+            return
+        }
+        /* 
+            Saving ammo is a two-step process:
+            1. Save the key of the ammo to the key database
+            2. Save the ammo object as a separate key/value pair in the ammo database
+        */
+
+        
+            
+
+        const allKeys:string = await AsyncStorage.getItem(A_KEY_DATABASE) // gets the object that holds all key values
+        const newKeys:string[] = allKeys == null ? [value.id] : [...JSON.parse(allKeys), value.id] // if its the first ammo to be saved, create an array with the id of the ammo. Otherwise, merge the key into the existing array
+        await AsyncStorage.setItem(A_KEY_DATABASE, JSON.stringify(newKeys)) // Save the key object
+        SecureStore.setItem(`${AMMO_DATABASE}_${value.id}`, JSON.stringify(value)) // Save the ammo
+        console.log(`Saved item ${JSON.stringify(value)} with key ${AMMO_DATABASE}_${value.id}`)
+        setSaveState(true)
+        setSnackbarText(`${value.designation} ${value.manufacturer ? value.manufacturer : ""} ${toastMessages.saved[language]}`)
+        onToggleSnackBar()
+        setNewAmmoOpen()
+        setCurrentAmmo(ammoData)
+        setSeeAmmoOpen()
+
+    }
+    
+    const pickImageAsync = async (indx:number) =>{
+        const permission: ImagePicker.MediaLibraryPermissionResponse = await ImagePicker.requestMediaLibraryPermissionsAsync()
+
+        if(!permission){
+            setGranted(false)
+            return
+        } else {
+            setGranted(true)
+        }
+
+        let result: ImagePicker.ImagePickerResult = await ImagePicker.launchImageLibraryAsync({
+            allowsEditing: true,
+            quality: 1
+        })
+
+        if(!result.canceled){
+            const newImage:string[] = selectedImage
+            if(newImage && newImage.length != 0){
+                newImage.splice(indx, 1, result.assets[0].uri)
+                setSelectedImage(newImage)
+                setAmmoData({...ammoData, images:newImage})
+            } else {
+                setSelectedImage([result.assets[0].uri])
+                setAmmoData({...ammoData, images:[result.assets[0].uri]})
+            }
+        }
+    }   
+
+    const pickCameraAsync = async (indx:number) =>{
+        const permission: ImagePicker.MediaLibraryPermissionResponse = await ImagePicker.requestMediaLibraryPermissionsAsync()
+
+        if(!permission){
+            setGranted(false)
+            return
+        } else {
+            setGranted(true)
+        }
+
+        let result: ImagePicker.ImagePickerResult = await ImagePicker.launchCameraAsync({
+            allowsEditing: true,
+            quality: 1
+        })
+
+        if(!result.canceled){
+            const newImage:string[] = selectedImage
+
+            if(newImage && newImage.length != 0){
+                newImage.splice(indx, 1, result.assets[0].uri)
+                setSelectedImage(newImage)
+                setAmmoData({...ammoData, images:newImage})
+            } else {
+                setSelectedImage([result.assets[0].uri])
+                setAmmoData({...ammoData, images:[result.assets[0].uri]})
+            }
+        }
+    }   
+
+    return(
+        <View style={{width: "100%", height: "100%", backgroundColor: "white"}}>
+            
+            <Appbar style={{width: "100%"}}>
+                <Appbar.BackAction  onPress={() => {saveState ? setNewAmmoOpen() : invokeAlert()}} />
+                <Appbar.Content title={newAmmoTitle[language]} />
+                <Appbar.Action icon="floppy" onPress={() => save({...ammoData, id: uuidv4(), images:selectedImage, createdAt: new Date(), lastModifiedAt: new Date()})} color={saveState ? "green" : "red"} />
+            </Appbar>
+
+            <View style={styles.container}>
+                <ScrollView style={{width: "100%"}}>
+                    <View>
+                        <ScrollView horizontal style={{width:"100%", aspectRatio: "21/10"}}>  
+                            {Array.from(Array(5).keys()).map((_, index) =>{
+                                return(
+                                    <View style={styles.imageContainer} key={`slide_${index}`}>
+                                        <ImageViewer isLightBox={false} selectedImage={selectedImage && selectedImage.length > 0 ? selectedImage[index] : null} />
+                                        <FAB
+                                            icon="camera"
+                                            style={styles.fab2}
+                                            onPress={()=>pickCameraAsync(index)}
+                                        />
+                                        <FAB
+                                            icon="image-multiple-outline"
+                                            style={styles.fab}
+                                            onPress={()=>pickImageAsync(index)}
+                                        />
+                                    </View>
+                                )
+                            })}
+                        </ScrollView>
+                    </View>
+                    <View style={{
+                        height: "100%",
+                        width: "100%",
+                        
+                    }}>
+                        <NewChipArea data={"status"} ammoData={ammoData} setAmmoData={setAmmoData}/>
+                        {ammoDataTemplate.map(data=>{
+                            return(
+                                <View 
+                                    id={data.name}
+                                    key={data.name}
+                                    style={{
+                                        display: "flex",
+                                        flexWrap: "nowrap",
+                                        flexDirection: "row",
+                                        width: "100%",
+                                        gap: 5,
+                                        
+                                }}>
+                                    
+                                    <NewText data={data.name} ammoData={ammoData} setAmmoData={setAmmoData} label={data[language]}/>
+                                </View>
+                            )
+                        })}
+                        <NewTextArea data={ammoRemarks.name} ammoData={ammoData} setAmmoData={setAmmoData}/>
+                    </View>
+                </ScrollView>
+            </View>
+            <Snackbar
+                visible={visible}
+                onDismiss={onDismissSnackBar}
+                action={{
+                label: 'OK',
+                onPress: () => {
+                    onDismissSnackBar()
+                },
+                }}>
+                {snackbarText}
+            </Snackbar>
+        </View>     
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        display: "flex",
+        flex: 1,
+        flexWrap: "wrap",
+        flexDirection: "column",
+        height: "100%",
+        width: "100%",
+        justifyContent: "center",
+        alignItems: "flex-start",
+        alignContent: "flex-start",
+        gap: 5,
+        padding: 5,
+    },
+    imageContainer: {
+        width: "100%",
+        aspectRatio: "18/10",
+        flexDirection: "column",
+        flex: 1,
+        marginRight: 5
+    },
+    button: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    fab: {
+        position: 'absolute',
+        margin: 16,
+        right: 0,
+        bottom: 0,
+    },
+    fab2: {
+    position: 'absolute',
+    margin: 16,
+    left: 0,
+    bottom: 0,
+    },
+  });
