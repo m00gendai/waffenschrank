22eb81be5aa3ea221fe927a8c49a41e22d52ce56
m00gendai <mrweber@gmx.ch>
2024-06-06 10:46:55 +0200

    New Ammo Collection component*

---
+++ components/AmmoCollection.tsx
@@ -0,0 +1,343 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
+import { Dimensions, ScrollView, StyleSheet, TouchableNativeFeedback, View, Text } from 'react-native';
+import { Appbar, Card, FAB, Menu, Switch, useTheme } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { AMMO_DATABASE, A_KEY_DATABASE, PREFERENCES, A_TAGS, defaultGridGap, defaultViewPadding } from '../configs';
+import { AmmoType, MenuVisibility } from '../interfaces';
+import * as SecureStore from "expo-secure-store"
+import { getIcon, doSortBy } from '../utils';
+import Animated, { FadeIn, FadeOut, SlideInDown, SlideOutDown } from 'react-native-reanimated';
+import { useViewStore } from '../stores/useViewStore';
+import { useAmmoStore } from '../stores/useAmmoStore';
+import { usePreferenceStore } from '../stores/usePreferenceStore';
+import { useTagStore } from '../stores/useTagStore';
+import { Checkbox } from 'react-native-paper';
+import newAmmo from './NewAmmo';
+import NewAmmo from './NewAmmo';
+import Ammo from './Ammo';
+
+export default function AmmoCollection(){
+
+  // TODO: Zustand SortIcon, Zustand SortOrder @ usePreferenceStore
+  // TODO: Zustand menuVisibility @ useViewStore
+  // Todo: Stricter typing ("stringA" | "stringB" instead of just string)
+
+  const [menuVisibility, setMenuVisibility] = useState<MenuVisibility>({sortBy: false, filterBy: false});
+  const [sortIcon, setSortIcon] = useState<string>("alphabetical-variant")
+  const [sortAscending, setSortAscending] = useState<boolean>(true)
+
+  const { ammoDbImport, displayAmmoAsGrid, setDisplayAmmoAsGrid, toggleDisplayAmmoAsGrid, sortAmmoBy, setSortAmmoBy } = usePreferenceStore()
+  const { mainMenuOpen, setMainMenuOpen, newAmmoOpen, setNewAmmoOpen, editAmmoOpen, setEditAmmoOpen, seeAmmoOpen, setSeeAmmoOpen } = useViewStore()
+  const { ammoCollection, setAmmoCollection, currentAmmo, setCurrentAmmo } = useAmmoStore()
+  const { ammo_tags, setAmmoTags, overWriteAmmoTags } = useTagStore()
+  const [isFilterOn, setIsFilterOn] = useState<boolean>(false);
+
+  const onToggleSwitch = () => setIsFilterOn(!isFilterOn);
+
+  const theme = useTheme();
+
+  const styles = StyleSheet.create({
+    container: {
+      width: "100%",
+      height: "100%",
+      flex: 1,
+      backgroundColor: theme.colors.background,
+      alignItems: 'flex-start',
+      justifyContent: 'flex-start',
+      flexWrap: "wrap",
+      flexDirection: "row",
+      gap: defaultGridGap,
+      padding: defaultViewPadding
+    },
+    fab: {
+      position: 'absolute',
+      margin: 16,
+      right: 0,
+      bottom: 0,
+    },
+    flagButton:{
+      fontSize: 20
+    }
+  });
+  
+    async function getKeys(){
+        const keys:string = await AsyncStorage.getItem(A_KEY_DATABASE)
+        if(keys == null){
+          return []
+        }
+        return JSON.parse(keys)
+      }
+      
+    useEffect(()=>{
+        async function getAmmo(){
+          const keys:string[] = await getKeys()
+          const ammunitions:AmmoType[] = await Promise.all(keys.map(async key =>{
+            const item:string = await SecureStore.getItemAsync(`${AMMO_DATABASE}_${key}`)
+            return JSON.parse(item)
+          }))
+          const preferences:string = await AsyncStorage.getItem(PREFERENCES)
+          const isPreferences = preferences === null ? null : JSON.parse(preferences)
+
+          const sortedAmmo = doSortBy(isPreferences === null ? "alphabetical" : isPreferences.sortAmmoBy === null ? "alphabetical" : isPreferences.sortAmmoBy, isPreferences == null? true : isPreferences.sortAmmoOrder === null ? true : isPreferences.sortOrder, ammunitions) as AmmoType[]
+          setAmmoCollection(sortedAmmo)
+        }
+        getAmmo()
+      },[newAmmoOpen, seeAmmoOpen, ammoDbImport])
+
+useEffect(()=>{
+  async function getPreferences(){
+    const preferences:string = await AsyncStorage.getItem(PREFERENCES)
+    const isPreferences = preferences === null ? null : JSON.parse(preferences)
+
+    const tagList: string = await AsyncStorage.getItem(A_TAGS)
+
+    const isTagList:{label: string, status: boolean}[] = tagList === null ? null : JSON.parse(tagList)
+   
+    setDisplayAmmoAsGrid(isPreferences === null ? true : isPreferences.displayAmmoAsGrid === null ? true : isPreferences.displayAmmoAsGrid)
+    setSortAmmoBy(isPreferences === null ? "alphabetical" : isPreferences.sortAmmoBy === null ? "alphabetical" : isPreferences.sortAmmoBy)
+    setSortIcon(getIcon((isPreferences === null ? "alphabetical" : isPreferences.sortAmmoBy === null ? "alphabetical" : isPreferences.sortAmmoBy)))
+
+    if(isTagList !== null && isTagList !== undefined){
+      Object.values(isTagList).map(tag =>{
+
+      setAmmoTags(tag)
+    }) 
+  }
+  
+  }
+  getPreferences()
+},[])
+
+        function handleAmmoCardPress(ammo){
+          setCurrentAmmo(ammo)
+          setSeeAmmoOpen()
+        }
+
+        async function handleSortBy(type: "alphabetical" | "chronological" | "caliber"){
+            setSortIcon(getIcon(type))
+            setSortAmmoBy(type)
+            const sortedAmmo = doSortBy(type, sortAscending, ammoCollection) as AmmoType[] 
+            setAmmoCollection(sortedAmmo)
+            const preferences:string = await AsyncStorage.getItem(PREFERENCES)
+            const newPreferences:{[key:string] : string} = preferences == null ? {"sortAmmoBy": type} : {...JSON.parse(preferences), "sortAmmoBy":type} 
+            await AsyncStorage.setItem(PREFERENCES, JSON.stringify(newPreferences))
+          }
+
+          function handleMenu(category: string, status: boolean){
+            setMenuVisibility({...menuVisibility, [category]: status})
+          }
+
+          async function handleSortOrder(){
+            setSortAscending(!sortAscending)
+            const sortedAmmo = doSortBy(sortAmmoBy, !sortAscending, ammoCollection) as AmmoType[] // called with !sortAscending due to the useState having still the old value
+            setAmmoCollection(sortedAmmo)
+            const preferences:string = await AsyncStorage.getItem(PREFERENCES)
+            const newPreferences:{[key:string] : string} = preferences == null ? {"sortOrder": !sortAscending} : {...JSON.parse(preferences), "sortOrder":!sortAscending} 
+            await AsyncStorage.setItem(PREFERENCES, JSON.stringify(newPreferences))
+          }
+        
+          async function handleDisplaySwitch(){
+            toggleDisplayAmmoAsGrid()
+            const preferences:string = await AsyncStorage.getItem(PREFERENCES)
+            const newPreferences:{[key:string] : string} = preferences == null ? {"displayAmmoAsGrid": !displayAmmoAsGrid} : {...JSON.parse(preferences), "displayAmmoAsGrid": !displayAmmoAsGrid} 
+            await AsyncStorage.setItem(PREFERENCES, JSON.stringify(newPreferences))
+          } 
+
+function sortCheckboxes(list:{label: string, status: boolean}[]){
+  return list.sort((a:{label: string, status: boolean}, b:{label: string, status: boolean}) =>{
+    const x = a.label
+    const y = b.label
+    return x > y ? 1 : x < y ? -1 : 0
+  })
+}
+
+function sortTags(list:{label: string, status: boolean}[]){
+
+  const removeDuplicates = (arr:{label:string,status:boolean}[]) => {
+    const map = new Map();
+    arr.forEach(item => {
+        if (!map.has(item.label)) {
+            map.set(item.label, item);
+        }
+    });
+    return Array.from(map.values());
+};
+
+const uniqueObjects = removeDuplicates(list);
+
+  return uniqueObjects.sort((a:{label: string, status: boolean}, b:{label: string, status: boolean}) =>{
+    const x = a.label
+    const y = b.label
+    return x > y ? 1 : x < y ? -1 : 0
+  })
+}
+
+async function handleFilterPress(tag:{label:string, status:boolean}){
+
+  const preferences:string = await AsyncStorage.getItem(A_TAGS)
+
+  const index = ammo_tags.findIndex(tagItem => tagItem.label === tag.label)
+
+  ammo_tags[index].status = !ammo_tags[index].status
+  overWriteAmmoTags(ammo_tags)
+
+            const newPreferences:{[key:string] : string} = preferences == null ? {"ammo_tags": ammo_tags} : {...JSON.parse(preferences), "ammo_tags":ammo_tags} 
+            await AsyncStorage.setItem(A_TAGS, JSON.stringify(newPreferences))
+ 
+}
+
+const activeTags = ammo_tags.filter(tag => tag.status === true)
+           const sortedTags = sortTags(ammo_tags)
+              const ammoList = ammoCollection.filter(ammo => activeTags.some(tag => ammo.tags?.includes(tag.label)))
+           
+
+    return(
+        <SafeAreaView 
+        style={{
+          width: "100%", 
+          height: "100%", 
+          flex: 1
+        }}
+      >
+
+        <Appbar style={{width: "100%", display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+         
+           <View  style={{display: "flex", flexDirection: "row", justifyContent: "flex-start"}}>
+            <Appbar.Action icon={"menu"} onPress={setMainMenuOpen} />
+          </View>
+          <View  style={{display: "flex", flexDirection: "row", justifyContent: "flex-end"}}>
+          <Menu
+            visible={menuVisibility.filterBy}
+            onDismiss={()=>handleMenu("filterBy", false)}
+            anchor={<Appbar.Action icon="filter" onPress={() =>{handleMenu("filterBy", true)}} />}
+            anchorPosition='bottom'
+            >
+            <View style={{padding: 5}}>
+              <View style={{display: "flex", flexDirection: "row", justifyContent: "flex-start", alignItems: "center"}}>
+                <Text>Filter:</Text>
+                <Switch value={isFilterOn} onValueChange={onToggleSwitch} />
+              </View>
+            
+              {sortedTags.map((tag, index)=>{
+        
+                return <Checkbox.Item key={`filter_${tag}_${index}`} label={tag.label} status={tag.status ? "checked" : "unchecked"} onPress={()=>handleFilterPress(tag)}/>
+              })}
+              </View>
+            </Menu>
+            <Appbar.Action icon={displayAmmoAsGrid ? "view-grid" : "format-list-bulleted-type"} onPress={handleDisplaySwitch} />
+            <Menu
+              visible={menuVisibility.sortBy}
+              onDismiss={()=>handleMenu("sortBy", false)}
+              anchor={<Appbar.Action icon={sortIcon} onPress={() => handleMenu("sortBy", true)} />}
+              anchorPosition='bottom'
+            >
+              <Menu.Item onPress={() => handleSortBy("alphabetical")} title="Alphabetisch" leadingIcon={getIcon("alphabetical")}/>
+              <Menu.Item onPress={() => handleSortBy("chronological")} title="Chronologisch" leadingIcon={getIcon("chronological")}/>
+             {/* <Menu.Item onPress={() => {}} title="Kaliber" leadingIcon={getIcon("caliber")}/> */}
+            </Menu>
+            <Appbar.Action icon={sortAscending ? "arrow-up" : "arrow-down"} onPress={() => handleSortOrder()} />
+          </View>
+        </Appbar>
+
+        <ScrollView 
+          contentContainerStyle={{
+            width: "100%", 
+            height: "100%", 
+            flexDirection: "column", 
+            flexWrap: "wrap"
+          }}
+        >
+          <View 
+            style={styles.container}
+          >
+            {ammoCollection.length != 0 && !isFilterOn ? ammoCollection.map(ammo =>{
+              return(
+                <TouchableNativeFeedback 
+                  key={ammo.id} 
+                  onPress={()=>handleAmmoCardPress(ammo)}
+                >
+                  <Card 
+                    style={{
+                      width: (Dimensions.get("window").width / (displayAmmoAsGrid ? 2 : 1)) - (defaultGridGap + (defaultViewPadding/2)),
+                    }}
+                  >
+                    <Card.Title titleStyle={ammo.currentStock && ammo.criticalStock ? {color: parseFloat(ammo.currentStock.toString()) <= parseFloat(ammo.criticalStock.toString()) ? "red" : "black"} : {}} title={`${ammo.designation} ${ammo.currentStock ? `(${ammo.currentStock})` : ""}`} subtitle={ammo.manufacturer && ammo.manufacturer.length != 0 ? ammo.manufacturer : " "} subtitleVariant='bodySmall' titleVariant='titleSmall' titleNumberOfLines={2} />
+                    {displayAmmoAsGrid ? 
+                    <Card.Cover 
+                      source={ammo.images && ammo.images.length != 0 ? { uri: ammo.images[0] } : require(`../assets//775788_several different realistic rifles and pistols on _xl-1024-v1-0.png`)} 
+                      style={{
+                        height: 100
+                      }}
+                    /> 
+                    : 
+                    null}
+                  </Card>
+                </TouchableNativeFeedback>
+              )
+            }) 
+            :
+            ammoCollection.length !== 0 && isFilterOn ? ammoList.map(ammo =>{
+              
+
+                
+                  return <TouchableNativeFeedback 
+                  key={ammo.id} 
+                  onPress={()=>handleAmmoCardPress(ammo)}
+                >
+                  <Card 
+                    style={{
+                      width: (Dimensions.get("window").width / (displayAmmoAsGrid ? 2 : 1)) - (defaultGridGap + (defaultViewPadding/2)),
+                    }}
+                  >
+                    <Card.Title titleStyle={ammo.currentStock && ammo.criticalStock ? {color: parseFloat(ammo.currentStock.toString()) <= parseFloat(ammo.criticalStock.toString()) ? "red" : "black"} : {}} title={`${ammo.designation} ${ammo.currentStock ? `(${ammo.currentStock})` : ""}`} subtitle={ammo.manufacturer && ammo.manufacturer.length != 0 ? ammo.manufacturer : " "} subtitleVariant='bodySmall' titleVariant='titleSmall' titleNumberOfLines={2} />
+                    {displayAmmoAsGrid ? 
+                    <Card.Cover 
+                      source={ammo.images && ammo.images.length != 0 ? { uri: ammo.images[0] } : require(`../assets//775788_several different realistic rifles and pistols on _xl-1024-v1-0.png`)} 
+                      style={{
+                        height: 100
+                      }}
+                    /> 
+                    : 
+                    null}
+                  </Card>
+                </TouchableNativeFeedback>
+                
+              
+            })
+              
+            :
+            null}
+          </View>
+        </ScrollView>
+        {newAmmoOpen ? 
+      <Animated.View entering={SlideInDown} exiting={SlideOutDown} style={{position: "absolute", left: 0, top: 0, right: 0, bottom: 0}}>
+        <SafeAreaView>
+          <NewAmmo />
+        </SafeAreaView>
+      </Animated.View> 
+      : 
+      null}
+        
+      {seeAmmoOpen ? 
+      <Animated.View entering={FadeIn} exiting={FadeOut} style={{position: "absolute", left: 0, top: 0, right: 0, bottom: 0}}>
+        <SafeAreaView>
+          <Ammo />
+        </SafeAreaView>
+      </Animated.View>
+      :
+      null}
+         {!seeAmmoOpen && !newAmmoOpen && !mainMenuOpen? 
+      <FAB
+        icon="plus"
+        style={styles.fab}
+        onPress={setNewAmmoOpen}
+        disabled={mainMenuOpen ? true : false}
+      /> 
+      : 
+      null}
+        
+      </SafeAreaView>
+    )
+}
+
