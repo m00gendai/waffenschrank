a5e322c5bca2291861f14411818d1b5f0ff117ee
m00gendai <mrweber@gmx.ch>
2024-06-20 14:01:59 +0200

    Enhanced Tag Modal Added previously used tags Ability to delete previously used tags Localized texts Added helper text what tags are about

--- components/NewChipArea.tsx
+++ components/NewChipArea.tsx
@@ -1,14 +1,15 @@
-import { View, TouchableNativeFeedback, Modal } from "react-native"
-import { newTags } from "../lib/textTemplates"
+import { View, TouchableNativeFeedback, Modal, ScrollView } from "react-native"
+import { deleteTagFromListAlert, newTags, tagModal } from "../lib/textTemplates"
 import { usePreferenceStore } from "../stores/usePreferenceStore"
 import { useGunStore } from "../stores/useGunStore"
 import { useAmmoStore } from "../stores/useAmmoStore"
-import { Button, Chip, IconButton, TextInput } from "react-native-paper"
+import { Button, Chip, IconButton, TextInput, Text, Dialog, Surface } from "react-native-paper"
 import { GunType, AmmoType } from "../interfaces"
 import { useState } from "react"
 import { useTagStore } from "../stores/useTagStore"
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { TAGS, A_TAGS } from "../configs"
+import { TAGS, A_TAGS, defaultViewPadding } from "../configs"
+import { BlurView } from "expo-blur"
 
 interface Props{
     data: string
@@ -23,41 +24,54 @@ export default function NewChipArea({data, gunData, setGunData, ammoData, setAmm
     const { language, theme } = usePreferenceStore()
     const { currentGun } = useGunStore()
     const { currentAmmo } = useAmmoStore()
-    const {tags, setTags, ammo_tags, setAmmoTags} = useTagStore()
+    const {tags, setTags, ammo_tags, setAmmoTags, overWriteAmmoTags, overWriteTags} = useTagStore()
 
     const [viewTagModal, setViewTagModal] = useState<boolean>(false)
     const [text, setText] = useState<string>("")
+    const [currentTag, setCurrentTag] = useState<string>("")
+    const [tagDeleteDialogVisible, toggleTagDeleteDialogVisible] = useState<boolean>(false)
 
-    async function saveNewTag(){
-        if(text === ""){
+    async function saveNewTag(tag: string | null){
+        const tagText:string = tag !== null ? tag : text
+        if(tag === null && text === ""){
             return
         }
+        if(gunData !== undefined && gunData.tags !== null && gunData.tags !== undefined && gunData.tags.length !== 0){
+            if(gunData.tags.includes(tagText)){
+                return
+            }
+        }
+        if(ammoData !== undefined && ammoData.tags !== null && ammoData.tags !== undefined && ammoData.tags.length !== 0){
+            if(ammoData.tags.includes(tagText)){
+                return
+            }
+        }
         gunData !== undefined && gunData && gunData.tags ? 
-        setGunData({...gunData, tags: [...gunData.tags, text]})
-        : gunData !== undefined && gunData && !gunData.tags ? setGunData({...gunData, tags: [text]})
+        setGunData({...gunData, tags: [...gunData.tags, tagText]})
+        : gunData !== undefined && gunData && !gunData.tags ? setGunData({...gunData, tags: [tagText]})
         : ammoData !== undefined && ammoData && ammoData.tags ?
-        setAmmoData({...ammoData, tags: [...ammoData.tags, text]})
-        : setAmmoData({...ammoData, tags: [text]})
+        setAmmoData({...ammoData, tags: [...ammoData.tags, tagText]})
+        : setAmmoData({...ammoData, tags: [tagText]})
 
         if(gunData !== undefined){
             const allTags:string = await AsyncStorage.getItem(TAGS) // gets the object that holds all key values
             const allTagsArray = allTags === null ? [] : JSON.parse(allTags)
-            if(!allTagsArray.some(element => element.label === text)){
-                setTags({label: text, status: true})
+            if(!allTagsArray.some(element => element.label === tagText)){
+                setTags({label: tagText, status: true})
 
-            
-                const newTags:{label:string, status:boolean}[] = allTagsArray.length === 0 ? [{label:text,status:true}] : [...allTagsArray, {label:text,status:true}] // if its the first gun to be saved, create an array with the id of the gun. Otherwise, merge the key into the existing array
+
+                const newTags:{label:string, status:boolean}[] = allTagsArray.length === 0 ? [{label:tagText,status:true}] : [...allTagsArray, {label:tagText,status:true}] // if its the first gun to be saved, create an array with the id of the gun. Otherwise, merge the key into the existing array
                 await AsyncStorage.setItem(TAGS, JSON.stringify(newTags)) // Save the key object
             }
         }
         if(ammoData !== undefined){
             const allTags:string = await AsyncStorage.getItem(A_TAGS) // gets the object that holds all key values
             const allTagsArray = allTags === null ? [] : JSON.parse(allTags)
-            if(!allTagsArray.some(element => element.label === text)){
-                setAmmoTags({label: text, status: true})
+            if(!allTagsArray.some(element => element.label === tagText)){
+                setAmmoTags({label: tagText, status: true})
     
             
-                const newTags:{label:string, status:boolean}[] = allTagsArray.length === 0 ? [{label:text,status:true}] : [...allTagsArray, {label:text,status:true}] // if its the first gun to be saved, create an array with the id of the gun. Otherwise, merge the key into the existing array
+                const newTags:{label:string, status:boolean}[] = allTagsArray.length === 0 ? [{label:tagText,status:true}] : [...allTagsArray, {label:tagText,status:true}] // if its the first gun to be saved, create an array with the id of the gun. Otherwise, merge the key into the existing array
                 await AsyncStorage.setItem(A_TAGS, JSON.stringify(newTags)) // Save the key object
             }
         }
@@ -79,6 +93,42 @@ export default function NewChipArea({data, gunData, setGunData, ammoData, setAmm
         }
     }
 
+    function addTagFromList(tag:string){
+        if(currentAmmo.tags !== null && currentAmmo.tags.length !== 0){
+            if(currentAmmo.tags.includes(tag)){
+                return
+            }
+        }
+        saveNewTag(tag)
+    }
+
+    function handleDeleteTagFromList(tag:string){
+        setCurrentTag(tag)
+        toggleTagDeleteDialogVisible(true)
+    }
+
+    async function deleteTagFromList(){
+        if(gunData !== undefined){
+            const allTags:string = await AsyncStorage.getItem(TAGS) // gets the object that holds all key values
+            const allTagsArray:{label:string, status:boolean}[] = allTags === null ? [] : JSON.parse(allTags)
+
+            const index = allTagsArray.findIndex(element => element.label === currentTag)
+            const newTags:{label:string, status:boolean}[] = allTagsArray.toSpliced(index, 1)
+            overWriteTags(newTags)
+            await AsyncStorage.setItem(TAGS, JSON.stringify(newTags)) // Save the key object
+            }
+        if(ammoData !== undefined){
+            const allTags:string = await AsyncStorage.getItem(A_TAGS) // gets the object that holds all key values
+            const allTagsArray:{label:string, status:boolean}[] = allTags === null ? [] : JSON.parse(allTags)
+            const index = allTagsArray.findIndex(element => element.label === currentTag)
+            const newTags:{label:string, status:boolean}[] = allTagsArray.toSpliced(index, 1)
+            overWriteAmmoTags(newTags)
+            await AsyncStorage.setItem(A_TAGS, JSON.stringify(newTags)) // Save the key object
+            }
+            deleteTag(currentTag)
+            toggleTagDeleteDialogVisible(false)
+        }
+
     return(
         <View >
         <TouchableNativeFeedback onPress={()=>setViewTagModal(true)} >
@@ -93,33 +143,74 @@ export default function NewChipArea({data, gunData, setGunData, ammoData, setAmm
             </View>
         </TouchableNativeFeedback>
         <Modal visible={viewTagModal} transparent>
-                    
+        <BlurView intensity={100}>    
         <View style={{width: "100%", height: "100%", display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", flexWrap: "wrap", backgroundColor: theme.colors.backdrop}}>
-            <View style={{width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
-                            <View style={{width: "85%", padding: 10, backgroundColor: theme.colors.background}}>
-                                <View style={{display: "flex", flexDirection: "row"}}>
+           <View style={{width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
+                            <View style={{width: "85%", padding: 10, backgroundColor: theme.colors.background, elevation: 10}}>
+                            <Text variant="titleMedium" style={{color: theme.colors.primary}}>{tagModal.title[language]}</Text>
+                            <Text variant="bodyMedium">{tagModal.subtitle[language]}</Text>
+                            <Surface style={{display: "flex", flexDirection: "row", flexWrap: "wrap", marginTop: 10, marginBottom: 10, justifyContent: "flex-start", padding: defaultViewPadding}}>
+                                <Text style={{width: "100%"}}>{tagModal.existingTags[language]}</Text>
+                                <View style={{height: 100, width: "100%"}}>
+                                    <ScrollView contentContainerStyle={{width: "100%", display: "flex", flexDirection: "row", flexWrap: "wrap", justifyContent: "flex-start"}}>
+                                    {Array.from(new Set(gunData !== undefined ? 
+                                        tags.map((tag, index) => 
+                                            <View key={`${tag.label}_${index}`} style={{padding: 5}}>
+                                                <Chip onPress={()=>addTagFromList(tag.label)} onClose={()=>handleDeleteTagFromList(tag.label)}>{tag.label}</Chip>
+                                            </View>) 
+                                        : 
+                                        ammo_tags.map((tag, index) => 
+                                            <View key={`${tag.label}_${index}`} style={{padding: 5}}>
+                                                <Chip onPress={()=>addTagFromList(tag.label)} onClose={()=>handleDeleteTagFromList(tag.label)}>{tag.label}</Chip>
+                                            </View>)
+                                    ))}
+                                    </ScrollView>
+                                    </View>
+                                </Surface>
+                                <Surface style={{display: "flex", flexDirection: "row", alignItems: "center", marginTop: 10, marginBottom: 10, padding: defaultViewPadding}}>
                                     <TextInput
-                                        label="Schlagwort eingeben"
+                                        label={tagModal.inputTags[language]}
                                         value={text}
                                         onChangeText={text => setText(text)}
                                         style={{flex: 8, marginRight: 10}}
                                         />
-                                        <IconButton mode="contained" icon={"floppy"} size={30} onPress={saveNewTag} style={{flex: 2, height: "100%", margin: 0}} />
-                                </View>
-                                <View style={{display: "flex", flexDirection: "row", flexWrap: "wrap", marginTop: 10, marginBottom: 10, justifyContent: "flex-start"}}>
+                                        <IconButton mode="contained" icon={"floppy"} size={30} onPress={()=>saveNewTag(null)} style={{ margin: 0}} />
+                                </Surface>
+                                <Surface style={{display: "flex", flexDirection: "row", flexWrap: "wrap", marginTop: 10, marginBottom: 10, justifyContent: "flex-start", padding: defaultViewPadding}}>
+                                <Text style={{width: "100%"}}>{tagModal.selectedTags[language]}</Text>
+                                <View style={{height: 100, width: "100%"}}>
+                                    <ScrollView contentContainerStyle={{width: "100%", display: "flex", flexDirection: "row", flexWrap: "wrap", justifyContent: "flex-start"}}>
                                     {gunData !== undefined && gunData?.tags?.map((tag, index) =>{
                                         return <View key={`${tag}_${index}`} style={{padding: 5}}><Chip onClose={()=>deleteTag(tag)}>{tag}</Chip></View>
                                     })}
                                     {ammoData !== undefined && ammoData?.tags?.map((tag, index) =>{
                                         return <View key={`${tag}_${index}`} style={{padding: 5}}><Chip onClose={()=>deleteTag(tag)}>{tag}</Chip></View>
                                     })}
+                                    </ScrollView>
                                 </View>
+                                </Surface>
                                 <Button mode="contained" onPress={()=>setViewTagModal(false)}>OK</Button>
                             </View>
                             </View>
                      
                     </View>
+                    <Dialog visible={tagDeleteDialogVisible} onDismiss={()=>toggleTagDeleteDialogVisible(false)}>
+            <Dialog.Title>
+                    {deleteTagFromListAlert.title[language]}
+                    </Dialog.Title>
+                    <Dialog.Content>
+                        <Text>{deleteTagFromListAlert.subtitle[language]}</Text>
+                    </Dialog.Content>
+                    <Dialog.Actions>
+                        <Button onPress={()=>deleteTagFromList()} icon="delete" buttonColor={theme.colors.errorContainer} textColor={theme.colors.onErrorContainer}>{deleteTagFromListAlert.yes[language]}</Button>
+                        <Button onPress={()=>toggleTagDeleteDialogVisible(false)} icon="cancel" buttonColor={theme.colors.secondary} textColor={theme.colors.onSecondary}>{deleteTagFromListAlert.no[language]}</Button>
+                    </Dialog.Actions>
+                </Dialog>
+                    </BlurView>
                 </Modal>
+
+                
+
         </View>
     )
 }
