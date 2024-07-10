7319bf892dfcc0a6fa9da18f74dffdfdf3ef5e94
m00gendai <mrweber@gmx.ch>
2024-07-10 12:50:04 +0200

    Adapted modals to ModalContainer

--- components/NewText.tsx
+++ components/NewText.tsx
@@ -8,6 +8,7 @@ import ColorPicker, { Panel1, Swatches, Preview, HueSlider } from 'reanimated-co
 import { calibers } from '../lib/caliberData';
 import { usePreferenceStore } from '../stores//usePreferenceStore';
 import { defaultModalBackdrop, defaultViewPadding, requiredFieldsAmmo, requiredFieldsGun } from '../configs';
+import ModalContainer from './ModalContainer';
 
 interface Props{
     data: string
@@ -169,101 +170,88 @@ function handleFocus(){
             </TouchableNativeFeedback>
             
             {/* DATE TIME PICKER */}
-            <Portal>
-            <Modal visible={showDateTime} onDismiss={()=>setShowDateTime(false)}>
-                <View style={{width: "100%", height: "100%", display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", flexWrap: "wrap", backgroundColor: defaultModalBackdrop}}>
-                    <View style={{width: "85%", height: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", flexWrap: "wrap"}}>
-
-                            <View style={{backgroundColor: theme.colors.background, width: "100%"}}>
-                                <DateTimePicker
-                                    mode="single"
-                                    locale="de"
-                                    date={date}
-                                    onChange={(params) => updateDate(params.date)}
-                                    selectedItemColor={theme.colors.primaryContainer}
-                                    calendarTextStyle={{color: theme.colors.onBackground}}
-                                    headerTextStyle={{color: theme.colors.primary}}
-                                    weekDaysTextStyle={{color: theme.colors.onBackground}}
-                                    headerButtonColor={theme.colors.primary}
-                                    monthContainerStyle={{backgroundColor: theme.colors.secondaryContainer, borderColor: theme.colors.secondaryContainer}}
-                                    yearContainerStyle={{backgroundColor: theme.colors.secondaryContainer, borderColor: theme.colors.secondaryContainer}}
-                                />
-                            
-                            <View style={{width: "100%", flexDirection: "row", flexWrap: "nowrap", justifyContent: "space-between"}}>
-                                <IconButton mode="contained" onPress={()=>confirmDate()} icon={"check"} style={{width: 50, backgroundColor: theme.colors.primary}} iconColor={theme.colors.onPrimary}/>
-                                <IconButton mode="contained" onPress={()=>cancelDate()} icon={"cancel"} style={{width: 50, backgroundColor: theme.colors.secondaryContainer}} />
-                                <IconButton mode="contained" onPress={()=>deleteDate()} icon={"delete"} style={{width: 50, backgroundColor: theme.colors.errorContainer}} iconColor={theme.colors.onErrorContainer}/>
-                            </View>
-                        </View>
-                    </View>
-                </View>
-            </Modal>
-            </Portal>
+            <ModalContainer 
+                title="Titel" 
+                subtitle={`text\nder\nmega\nlange\nist\nder\nmega\nlange\nist\nder\nmega\nlange\nist\nder\nmega\nlange\nist\nder\nmega\nlange\nist\nder\nmega\nlange\nist\nder\nmega\nlange\nist`} 
+                visible={showDateTime} 
+                setVisible={setShowDateTime}
+                content={<DateTimePicker
+                            mode="single"
+                            locale="de"
+                            date={date}
+                            onChange={(params) => updateDate(params.date)}
+                            selectedItemColor={theme.colors.primaryContainer}
+                            calendarTextStyle={{color: theme.colors.onBackground}}
+                            headerTextStyle={{color: theme.colors.primary}}
+                            weekDaysTextStyle={{color: theme.colors.onBackground}}
+                            headerButtonColor={theme.colors.primary}
+                            monthContainerStyle={{backgroundColor: theme.colors.secondaryContainer, borderColor: theme.colors.secondaryContainer}}
+                            yearContainerStyle={{backgroundColor: theme.colors.secondaryContainer, borderColor: theme.colors.secondaryContainer}}
+                        />}
+                buttonACK={<IconButton mode="contained" onPress={()=>confirmDate()} icon={"check"} style={{width: 50, backgroundColor: theme.colors.primary}} iconColor={theme.colors.onPrimary}/>}
+                buttonCNL={<IconButton mode="contained" onPress={()=>cancelDate()} icon={"cancel"} style={{width: 50, backgroundColor: theme.colors.secondaryContainer}} />}
+                buttonDEL={<IconButton mode="contained" onPress={()=>deleteDate()} icon={"delete"} style={{width: 50, backgroundColor: theme.colors.errorContainer}} iconColor={theme.colors.onErrorContainer}/>}
+             />
         
             {/* COLOR PICKER */}
-            <Portal>
-            <Modal visible={showModal} onDismiss={()=>setShowModal(false)}>
-                <View style={{width: "100%", height: "100%", display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", flexWrap: "wrap", backgroundColor: defaultModalBackdrop}}>
-                    <View style={{width: "85%", height: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", flexWrap: "wrap"}}>
-                        <View style={{backgroundColor: theme.colors.background, width: "100%"}}>
-                            <ColorPicker style={{ width: '100%', padding: 10 }} value={gunData ? gunData[data] : "#000"} onComplete={onSelectColor}>
-                                <Preview style={{marginBottom: 10}} />
-                                <Panel1 style={{marginBottom: 10}} />
-                                <HueSlider style={{marginBottom: 20, marginTop: 20}} />
-                                <Swatches colors={["#000000", "#c0c0c0", "#e0e0e0", "#818589", "#6b8e23", "#877348", "#f6d7b0", "#ff69b4", "#ffc5cb", "#dd8a3c", "#56301d"]}/>
-                            </ColorPicker>
-                            <View style={{width: "100%", marginTop: 10, display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
-                                <IconButton icon="check" onPress={() => handleColorConfirm()} style={{width: 50, backgroundColor: theme.colors.primary}} iconColor={theme.colors.onPrimary}/>
-                                <IconButton icon="cancel" onPress={() => handleColorCancel()} style={{width: 50, backgroundColor: theme.colors.secondaryContainer}} iconColor={theme.colors.onSecondaryContainer} />
-                            </View>
-                        </View>
-                    </View>
-                </View>
-            </Modal>
-            </Portal>
-
+            <ModalContainer
+                title={`ColorPicker`}
+                subtitle={`Pick Colors`}
+                visible={showModal}
+                setVisible={setShowModal}
+                content={<ColorPicker style={{ width: '100%', padding: 10 }} value={gunData ? gunData[data] : "#000"} onComplete={onSelectColor}>
+                <Preview style={{marginBottom: 10}} />
+                <Panel1 style={{marginBottom: 10}} />
+                <HueSlider style={{marginBottom: 20, marginTop: 20}} />
+                <Swatches colors={["#000000", "#c0c0c0", "#e0e0e0", "#818589", "#6b8e23", "#877348", "#f6d7b0", "#ff69b4", "#ffc5cb", "#dd8a3c", "#56301d"]}/>
+            </ColorPicker>}
+            buttonACK={<IconButton icon="check" onPress={() => handleColorConfirm()} style={{width: 50, backgroundColor: theme.colors.primary}} iconColor={theme.colors.onPrimary}/>}
+            buttonCNL={<IconButton icon="cancel" onPress={() => handleColorCancel()} style={{width: 50, backgroundColor: theme.colors.secondaryContainer}} iconColor={theme.colors.onSecondaryContainer} />}
+            buttonDEL={null}
+            />
 
             {/* CALIBER PICKER */}
-            <Portal>
-            <Modal visible={showModalCaliber} onDismiss={()=>setShowModalCaliber(false)}>
-                <View style={{width: "100%", height: "100%", display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", flexWrap: "wrap", backgroundColor: defaultModalBackdrop}}>
-                    <View style={{width: "85%", height: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", flexWrap: "wrap"}}>
-                        <View style={{backgroundColor: theme.colors.background, width: "100%", height: "90%"}}>
-                            <List.Section style={{flex: 1}}>
-                                <View style={{padding: defaultViewPadding}}>
-                                    <Text variant="titleMedium" style={{color: theme.colors.primary}}>{`${Array.isArray(activeCaliber) && activeCaliber.length !== 0 ? activeCaliber.join("\n") : "Kaliber auswählen"}`}</Text>
-                               </View>
-                                <ScrollView>
-                                    {calibers.map((caliber, index) =>{
+            <ModalContainer
+                title={`ColorPicker`}
+                subtitle={`Pick Colors`}
+                visible={showModalCaliber}
+                setVisible={setShowModalCaliber}
+                content={<List.Section style={{width: "100%", flexDirection: "column", height: "100%"}}>
+                    <ScrollView style={{height: "20%", width: "100%", backgroundColor: theme.colors.background}}>
+                    <Text variant="titleMedium" style={{color: theme.colors.primary, padding: defaultViewPadding}}>{`${Array.isArray(activeCaliber) && activeCaliber.length !== 0 ? activeCaliber.join("\n") : "Kaliber auswählen"}`}</Text>
+               </ScrollView>
+                <ScrollView style={{height: "80%", width: "100%", backgroundColor: "yellow"}}>
+                {calibers.map((caliber, index) =>{
+                        return(
+                            <List.Accordion
+                                title={caliber.range}
+                                key={caliber.range}
+                                style={{
+                                    backgroundColor: theme.colors.secondaryContainer,
+                                }}
+                            >
+                                <View style={{backgroundColor: theme.colors.tertiaryContainer}}>
+                                    {caliber.variants.map((variant, index)=>{
                                         return(
-                                            <List.Accordion
-                                                title={caliber.range}
-                                                key={caliber.range}
-                                                style={{
-                                                    backgroundColor: theme.colors.secondaryContainer,
-                                                }}
-                                            >
-                                                <View style={{backgroundColor: theme.colors.tertiaryContainer}}>
-                                                    {caliber.variants.map((variant, index)=>{
-                                                        return(
-                                                            <List.Item key={`${variant.name}_${index}`} title={variant.name} titleStyle={{color: activeCaliber !== undefined && activeCaliber !== null && activeCaliber.length !== 0 && activeCaliber.includes(variant.name) ? theme.colors.onTertiary : theme.colors.onTertiaryContainer}} onPress={()=>handleCaliberItemSelect(variant.name)} style={{backgroundColor: activeCaliber.includes(variant.name) ? theme.colors.tertiary : "transparent"}}/>
-                                                        )
-                                                    })}
-                                                </View>
-                                            </List.Accordion>
+                                            <List.Item key={`${variant.name}_${index}`} title={variant.name} titleStyle={{color: activeCaliber !== undefined && activeCaliber !== null && activeCaliber.length !== 0 && activeCaliber.includes(variant.name) ? theme.colors.onTertiary : theme.colors.onTertiaryContainer}} onPress={()=>handleCaliberItemSelect(variant.name)} style={{backgroundColor: activeCaliber.includes(variant.name) ? theme.colors.tertiary : "transparent"}}/>
                                         )
                                     })}
-                                </ScrollView>
-                            </List.Section>
-                            <View style={{width: "100%", marginTop: 10, display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
-                                <IconButton icon="check" onPress={() => handleCaliberSelectConfirm()} style={{width: 50, backgroundColor: theme.colors.primary}} iconColor={theme.colors.onPrimary}/>
-                                <IconButton icon="cancel" onPress={() => handleCaliberSelectCancel()} style={{width: 50, backgroundColor: theme.colors.secondaryContainer}} iconColor={theme.colors.onSecondaryContainer} />
-                            </View>
-                        </View>
-                    </View>
-                </View>
-            </Modal>
-            </Portal>
+                                </View>
+
+                            </List.Accordion>
+                        )
+                    })}
+                </ScrollView>
+            </List.Section>}
+            buttonACK={<IconButton icon="check" onPress={() => handleCaliberSelectConfirm()} style={{width: 50, backgroundColor: theme.colors.primary}} iconColor={theme.colors.onPrimary}/>}
+            buttonCNL={<IconButton icon="cancel" onPress={() => handleCaliberSelectCancel()} style={{width: 50, backgroundColor: theme.colors.secondaryContainer}} iconColor={theme.colors.onSecondaryContainer} />}
+            buttonDEL={null}
+            />
+
         </View>
     )
 }
+
+/*
+
+                    */
