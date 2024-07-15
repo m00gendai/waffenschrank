9016453a24d606d874d1ab866f21894a858c2804
m00gendai <mrweber@gmx.ch>
2024-07-15 05:17:59 +0200

    Added Dialog for helper text instead of scrollview ScrollView took too much screen real estate and was awkward to use.

--- components/ModalContainer.tsx
+++ components/ModalContainer.tsx
@@ -1,7 +1,8 @@
-import { Modal, Portal, Text } from "react-native-paper";
+import { Button, Dialog, IconButton, Modal, Portal, Text } from "react-native-paper";
 import { defaultModalBackdrop, defaultViewPadding } from "../configs";
 import { ScrollView, View } from "react-native";
 import { usePreferenceStore } from "../stores/usePreferenceStore";
+import { useState } from "react";
 
 interface Props{
     visible: boolean
@@ -17,17 +18,15 @@ interface Props{
 export default function ModalContainer({visible, setVisible, title, subtitle, content, buttonACK, buttonCNL, buttonDEL}:Props){
 
     const { language, theme } = usePreferenceStore()
+    const [seeInfo, toggleSeeInfo] = useState<boolean>(false)
     
     return(
-        <Portal>
+                <Portal>
             <Modal visible={visible} onDismiss={()=>setVisible(false)}>
                 <View style={{width: "100%", height: "100%", display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", alignContent: "center", flexWrap: "wrap", backgroundColor: defaultModalBackdrop}}>
                     <View style={{borderRadius: 25, width: "85%", height: "85%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "flex-start", flexWrap: "wrap", backgroundColor: theme.colors.background}}>
-                        <View style={{borderTopLeftRadius: 25, borderTopRightRadius: 25, width: "100%", flex: 2.5, backgroundColor: theme.colors.background, borderBottomColor: theme.colors.primary, borderBottomWidth: 1, marginBottom: 5}}>
-                            <Text variant="titleLarge" style={{color: theme.colors.primary, padding: defaultViewPadding}}>{title}</Text>
-                            <ScrollView>
-                                <Text variant="bodySmall" style={{width: "100%", padding: defaultViewPadding, backgroundColor: theme.colors.background}}>{subtitle}</Text>
-                            </ScrollView>
+                        <View style={{borderTopLeftRadius: 25, borderTopRightRadius: 25, width: "100%", backgroundColor: theme.colors.background, borderBottomColor: theme.colors.primary, borderBottomWidth: 1, marginBottom: 5}}>
+                            <View style={{display: "flex", flexDirection: "row"}}><Text variant="titleLarge" style={{color: theme.colors.primary, padding: defaultViewPadding, flex: 9}}>{title}</Text><IconButton style={{flex: 1}} icon="help-circle-outline" onPress={()=>toggleSeeInfo(true)}/></View>
                         </View>
                         <View style={{flex: 8, flexDirection: "row", justifyContent: "center", alignItems: "center"}}>
                         {content}
@@ -39,7 +38,19 @@ export default function ModalContainer({visible, setVisible, title, subtitle, co
                         </View>
                     </View>
                 </View>
+                <Dialog visible={seeInfo}>
+             <Dialog.Content>
+               <Text variant="bodyMedium">{subtitle}</Text>
+             </Dialog.Content>
+             <Dialog.Actions>
+          <Button onPress={() => toggleSeeInfo(false)}>OK</Button>
+        </Dialog.Actions>
+             </Dialog>
             </Modal>
+            
+            
         </Portal>
+
+           
     )
 }
