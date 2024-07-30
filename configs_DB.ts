7ae1b877763b3092231d841496d0ad0794ef5911
m00gendai <mrweber@gmx.ch>
2024-07-31 00:47:26 +0200

    Unstaged too early

---
+++ configs_DB.ts
@@ -0,0 +1,15 @@
+/* Gun Collection databases */
+export const KEY_DATABASE:string = "key0009"
+export const GUN_DATABASE:string = "gun0009"
+export const TAGS:string = "tag0013"
+
+/* Ammo Collection databases */
+export const A_KEY_DATABASE:string = "akey0009"
+export const AMMO_DATABASE:string = "ammo0009"
+export const A_TAGS:string = "atag0012"
+
+/* Preferences database is shared */
+export const PREFERENCES:string = "pre0018"
+
+/* Range Log Database */
+export const RANGELOG:string = "rlog0001"
