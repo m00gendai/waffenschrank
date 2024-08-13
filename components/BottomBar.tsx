9f7bb757b776678a8f9324fd4bd6c4f4ae0c609c
m00gendai <mrweber@gmx.ch>
2024-08-13 12:09:40 +0200

    BottomBar button color depending on current screen

--- components/BottomBar.tsx
+++ components/BottomBar.tsx
@@ -4,6 +4,7 @@ import { usePreferenceStore } from "../stores/usePreferenceStore";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from '@react-navigation/stack';
 import { tabBarLabels } from "../lib/textTemplates";
+import { defaultBottomBarHeight } from "../configs";
 
 type RootStackParamList = {
   GunCollection: undefined;
@@ -13,8 +14,12 @@ type RootStackParamList = {
 
 type BottomBarNavigationProp = StackNavigationProp<RootStackParamList>;
 
+interface Props{
+  screen?: string
+}
 
-export default function BottomBar(){
+
+export default function BottomBar({screen}){
 
     type RootStackParamList = {
         GunCollection: undefined;
@@ -27,14 +32,14 @@ export default function BottomBar(){
     const navigation = useNavigation<BottomBarNavigationProp>()
     
     return(
-        <View style={{width: "100%", height: 60, backgroundColor: theme.colors.backdrop, flexDirection: "row", justifyContent: "space-around", alignItems: "center"}}>
+        <View style={{width: "100%", height: defaultBottomBarHeight, backgroundColor: theme.colors.surface, flexDirection: "row", justifyContent: "space-around", alignItems: "center"}}>
       <TouchableOpacity onPress={()=>navigation.navigate("GunCollection")} style={{ alignItems: 'center' }}>
-        <Icon source="pistol" size={24} color={theme.colors.primary} />
-        <Text style={{ color: theme.colors.primary, marginTop: 4 }}>{tabBarLabels.gunCollection[language]}</Text>
+        <Icon source="pistol" size={24} color={screen === "GunCollection" ? theme.colors.primary : theme.colors.secondary} />
+        <Text style={{ color: screen === "GunCollection" ? theme.colors.primary : theme.colors.secondary, marginTop: 4 }}>{tabBarLabels.gunCollection[language]}</Text>
        </TouchableOpacity>
        <TouchableOpacity onPress={()=>navigation.navigate("AmmoCollection")} style={{ alignItems: 'center' }}>
-        <Icon source="bullet" size={24} color={theme.colors.primary} />
-        <Text style={{ color: theme.colors.primary, marginTop: 4 }}>{tabBarLabels.ammoCollection[language]}</Text>
+        <Icon source="bullet" size={24} color={screen === "AmmoCollection" ? theme.colors.primary : theme.colors.secondary} />
+        <Text style={{ color: screen === "AmmoCollection" ? theme.colors.primary : theme.colors.secondary, marginTop: 4 }}>{tabBarLabels.ammoCollection[language]}</Text>
        </TouchableOpacity>
           </View>
     )
