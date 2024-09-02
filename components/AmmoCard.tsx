0343cb5dd7c4fc7baf1f0fdd20980b8714b9313a
m00gendai <mrweber@gmx.ch>
2024-09-02 19:14:21 +0200

    Long Press Dialog

--- components/AmmoCard.tsx
+++ components/AmmoCard.tsx
@@ -1,6 +1,6 @@
-import { Dimensions, ScrollView, StyleSheet, TouchableNativeFeedback, View, Text } from 'react-native';
+import { Dimensions, ScrollView, StyleSheet, TouchableNativeFeedback, View, Pressable } from 'react-native';
 import { AmmoType, StackParamList } from '../interfaces';
-import { Avatar, Badge, Button, Card, IconButton, TouchableRipple } from 'react-native-paper';
+import { Avatar, Badge, Button, Card, Dialog, Icon, IconButton, Text, Modal, Portal, TouchableRipple } from 'react-native-paper';
 import { usePreferenceStore } from '../stores/usePreferenceStore';
 import { dateLocales, defaultGridGap, defaultViewPadding } from '../configs';
 import { ammoDataTemplate } from '../lib/ammoDataTemplate';
@@ -8,6 +8,11 @@ import { useAmmoStore } from '../stores/useAmmoStore';
 import { useViewStore } from '../stores/useViewStore';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { useState } from 'react';
+import { AMMO_DATABASE, KEY_DATABASE } from '../configs_DB';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as SecureStore from "expo-secure-store"
+import { ammoDeleteAlert, gunDeleteAlert, longPressActions } from '../lib/textTemplates';
 
 interface Props{
     ammo: AmmoType
@@ -20,6 +25,10 @@ export default function AmmoCard({ammo}:Props){
     const { ammoCollection, setAmmoCollection, currentAmmo, setCurrentAmmo } = useAmmoStore()
     const navigation = useNavigation<NativeStackNavigationProp<StackParamList>>()
 
+    const [longVisible, setLongVisible] = useState<boolean>(false)
+    const [dialogVisible, toggleDialogVisible] = useState<boolean>(false)
+
+
     function handleStockButtonPress(ammo:AmmoType){
         setCurrentAmmo(ammo)
         navigation.navigate("QuickStock")
@@ -30,10 +39,40 @@ export default function AmmoCard({ammo}:Props){
         navigation.navigate("Ammo")
       }
 
+      function meloveyoulongtime(){
+        console.log("TWO DOLLA")
+        setLongVisible(true)
+        setCurrentAmmo(ammo)
+      }
+
+      function handleClone(){
+        setLongVisible(false)
+        navigation.navigate("NewAmmo")
+        
+      }
+
+      async function deleteItem(ammo:AmmoType){
+        // Deletes gun in gun database
+        await SecureStore.deleteItemAsync(`${AMMO_DATABASE}_${ammo.id}`)
+
+        // retrieves gun ids from key database and removes the to be deleted id
+        const keys:string = await AsyncStorage.getItem(KEY_DATABASE)
+        const keyArray: string[] = JSON.parse(keys)
+        const newKeys: string[] = keyArray.filter(key => key != ammo.id)
+        AsyncStorage.setItem(KEY_DATABASE, JSON.stringify(newKeys))
+        const index:number = ammoCollection.indexOf(ammo)
+        const newCollection:AmmoType[] = ammoCollection.toSpliced(index, 1)
+        setAmmoCollection(newCollection)
+        toggleDialogVisible(false)
+        setLongVisible(false)
+    }
+
     return(
+        <>
         <TouchableNativeFeedback 
                 key={ammo.id} 
                 onPress={()=>handleAmmoCardPress(ammo)}
+                onLongPress={()=>meloveyoulongtime()}
               >
         <Card 
             style={{
@@ -129,5 +168,40 @@ export default function AmmoCard({ammo}:Props){
             </View>}
         </Card>
         </TouchableNativeFeedback>
+
+<Portal>
+<Modal visible={longVisible} onDismiss={()=>setLongVisible(false)}>
+    <View style={{width: "85%", alignSelf: "center", backgroundColor: theme.colors.background, padding: defaultViewPadding, flexDirection: "row"}}>
+    <View style={{width: "100%", display: "flex", height: "100%", flexDirection: "row", justifyContent: "space-around"}}>
+    <Pressable onPress={()=>handleClone()} style={{ alignItems: 'center' }}>
+        <Icon source="content-duplicate" size={48} />
+        <Text style={{marginTop: defaultViewPadding}}>{longPressActions.clone[language]}</Text>
+   </Pressable>
+   <Pressable onPress={()=>toggleDialogVisible(true)} style={{ alignItems: 'center' }}>
+        <Icon source="delete" size={48} color={theme.colors.error}/>
+        <Text style={{marginTop: defaultViewPadding, color: theme.colors.error}}>{longPressActions.delete[language]}</Text>
+   </Pressable>
+   </View>
+    </View>
+    
+</Modal>
+</Portal>
+
+<Portal>
+                    <Dialog visible={dialogVisible} onDismiss={()=>toggleDialogVisible(!dialogVisible)}>
+                        <Dialog.Title>
+                        {`${ammo.designation === null ? "" : ammo.designation === undefined ? "" : ammo.designation} ${ammoDeleteAlert.title[language]}`}
+                        </Dialog.Title>
+                        <Dialog.Content>
+                            <Text>{`${ammoDeleteAlert.subtitle[language]}`}</Text>
+                        </Dialog.Content>
+                        <Dialog.Actions>
+                            <Button onPress={()=>deleteItem(currentAmmo)} icon="delete" buttonColor={theme.colors.errorContainer} textColor={theme.colors.onErrorContainer}>{gunDeleteAlert.yes[language]}</Button>
+                            <Button onPress={()=>toggleDialogVisible(!dialogVisible)} icon="cancel" buttonColor={theme.colors.secondary} textColor={theme.colors.onSecondary}>{gunDeleteAlert.no[language]}</Button>
+                        </Dialog.Actions>
+                    </Dialog>
+                </Portal>      
+
+    </>
     )
 }
--- components/GunCard.tsx
+++ components/GunCard.tsx
@@ -1,6 +1,6 @@
-import { Dimensions, TouchableNativeFeedback, View } from 'react-native';
+import { Dimensions, Pressable, TouchableNativeFeedback, View } from 'react-native';
 import { GunType, StackParamList } from '../interfaces';
-import { Card, IconButton } from 'react-native-paper';
+import { Button, Card, Dialog, Icon, IconButton, Modal, Portal, Text } from 'react-native-paper';
 import { usePreferenceStore } from '../stores/usePreferenceStore';
 import { defaultGridGap, defaultViewPadding } from '../configs';
 import { useViewStore } from '../stores/useViewStore';
@@ -8,6 +8,11 @@ import { useGunStore } from '../stores/useGunStore';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { checkDate } from '../utils';
+import { useState } from 'react';
+import { GUN_DATABASE, KEY_DATABASE } from '../configs_DB';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as SecureStore from "expo-secure-store"
+import { gunDeleteAlert, longPressActions } from '../lib/textTemplates';
 
 interface Props{
     gun: GunType
@@ -20,6 +25,9 @@ export default function GunCard({gun}:Props){
     const { gunCollection, setGunCollection, currentGun, setCurrentGun } = useGunStore()  
     const navigation = useNavigation<NativeStackNavigationProp<StackParamList>>()
 
+    const [longVisible, setLongVisible] = useState<boolean>(false)
+    const [dialogVisible, toggleDialogVisible] = useState<boolean>(false)
+
     function handleGunCardPress(gun){
         setCurrentGun(gun)
         navigation.navigate("Gun")
@@ -30,10 +38,40 @@ export default function GunCard({gun}:Props){
         navigation.navigate("QuickShot")
       }
 
+      function meloveyoulongtime(){
+        console.log("TWO DOLLA")
+        setLongVisible(true)
+        setCurrentGun(gun)
+      }
+
+      function handleClone(){
+        setLongVisible(false)
+        navigation.navigate("NewGun")
+        
+      }
+
+      async function deleteItem(gun:GunType){
+        // Deletes gun in gun database
+        await SecureStore.deleteItemAsync(`${GUN_DATABASE}_${gun.id}`)
+
+        // retrieves gun ids from key database and removes the to be deleted id
+        const keys:string = await AsyncStorage.getItem(KEY_DATABASE)
+        const keyArray: string[] = JSON.parse(keys)
+        const newKeys: string[] = keyArray.filter(key => key != gun.id)
+        AsyncStorage.setItem(KEY_DATABASE, JSON.stringify(newKeys))
+        const index:number = gunCollection.indexOf(gun)
+        const newCollection:GunType[] = gunCollection.toSpliced(index, 1)
+        setGunCollection(newCollection)
+        toggleDialogVisible(false)
+        setLongVisible(false)
+    }
+
     return(
+        <>
         <TouchableNativeFeedback 
                 key={gun.id} 
                 onPress={()=>handleGunCardPress(gun)}
+                onLongPress={()=>meloveyoulongtime()}
               >
         <Card 
             style={{
@@ -115,5 +153,40 @@ export default function GunCard({gun}:Props){
             </View>}
         </Card>
         </TouchableNativeFeedback>
+
+<Portal>
+    <Modal visible={longVisible} onDismiss={()=>setLongVisible(false)}>
+        <View style={{width: "85%", alignSelf: "center", backgroundColor: theme.colors.background, padding: defaultViewPadding, flexDirection: "row"}}>
+        <View style={{width: "100%", display: "flex", height: "100%", flexDirection: "row", justifyContent: "space-around"}}>
+        <Pressable onPress={()=>handleClone()} style={{ alignItems: 'center' }}>
+            <Icon source="content-duplicate" size={48} />
+            <Text style={{marginTop: defaultViewPadding}}>{longPressActions.clone[language]}</Text>
+       </Pressable>
+       <Pressable onPress={()=>toggleDialogVisible(true)} style={{ alignItems: 'center' }}>
+            <Icon source="delete" size={48} color={theme.colors.error}/>
+            <Text style={{marginTop: defaultViewPadding, color: theme.colors.error}}>{longPressActions.delete[language]}</Text>
+       </Pressable>
+       </View>
+        </View>
+        
+    </Modal>
+</Portal>
+
+<Portal>
+                        <Dialog visible={dialogVisible} onDismiss={()=>toggleDialogVisible(!dialogVisible)}>
+                            <Dialog.Title>
+                            {`${gun.model === null ? "" : gun.model === undefined ? "" : gun.model} ${gunDeleteAlert.title[language]}`}
+                            </Dialog.Title>
+                            <Dialog.Content>
+                                <Text>{`${gunDeleteAlert.subtitle[language]}`}</Text>
+                            </Dialog.Content>
+                            <Dialog.Actions>
+                                <Button onPress={()=>deleteItem(currentGun)} icon="delete" buttonColor={theme.colors.errorContainer} textColor={theme.colors.onErrorContainer}>{gunDeleteAlert.yes[language]}</Button>
+                                <Button onPress={()=>toggleDialogVisible(!dialogVisible)} icon="cancel" buttonColor={theme.colors.secondary} textColor={theme.colors.onSecondary}>{gunDeleteAlert.no[language]}</Button>
+                            </Dialog.Actions>
+                        </Dialog>
+                    </Portal>      
+
+        </>
     )
 }
