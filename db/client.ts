79c710e77c2bf51d9716a1407a8c4fbb9a70d13e
m00gendai <mrweber@gmx.ch>
2024-09-18 10:59:02 +0200

    Export for expo for expo studio

--- db/client.ts
+++ db/client.ts
@@ -1,5 +1,6 @@
 import { drizzle } from "drizzle-orm/expo-sqlite"
+import * as SQLite from "expo-sqlite"
 import { openDatabaseSync } from "expo-sqlite/next"
 
-const expo = openDatabaseSync("test_db5.db", {enableChangeListener: true})
+export const expo = openDatabaseSync("test_db21.db", {enableChangeListener: true})
 export const db = drizzle(expo)
