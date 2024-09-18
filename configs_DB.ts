3f777e6c438f07fef649c57d7b2544402c61cc8c
m00gendai <mrweber@gmx.ch>
2024-09-18 13:27:50 +0200

    Comments

--- configs_DB.ts
+++ configs_DB.ts
@@ -1,3 +1,10 @@
+/*
+
+These are legacy values. Databases are migrated to SQLite.
+They remain here for legacy support.
+
+*/
+
 /* Gun Collection databases */
 export const KEY_DATABASE:string = "key0009" // 0009
 export const GUN_DATABASE:string = "gun0009" // 0009
@@ -8,8 +15,21 @@ export const A_KEY_DATABASE:string = "akey0009" // 0009
 export const AMMO_DATABASE:string = "ammo0009" // 0009
 export const A_TAGS:string = "atag0012" // 0012
 
+/*
+
+Preferences Database will still be used 
+
+*/
+
 /* Preferences database is shared */
 export const PREFERENCES:string = "pre0018" // 0018
+
+/*
+
+Range Log database was and will remain unused.
+TODO: Remove RANGELOG
+
+*/
 
 /* Range Log Database */
 export const RANGELOG:string = "rlog0001" // 0001
