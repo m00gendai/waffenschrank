4f76261cba1b960bb3a76fa7f743cedada9b7f6e
m00gendai <mrweber@gmx.ch>
2024-09-21 10:50:11 +0200

    Refactored for new schema

--- components/NewCheckboxArea.tsx
+++ components/NewCheckboxArea.tsx
@@ -4,47 +4,20 @@ import { GunType } from "../interfaces"
 import { useState } from "react";
 import { checkBoxes } from "../lib/gunDataTemplate";
 import { usePreferenceStore } from "../stores/usePreferenceStore";
+import { check } from "drizzle-orm/mysql-core";
 
 interface Props{
-    data: string
     gunData: GunType
     setGunData: React.Dispatch<React.SetStateAction<GunType>>
 }
 
-export default function NewCheckboxArea({data, gunData, setGunData}: Props){
-
-    const convertArrayToObject = (array:{ name: string; de: string; en: string; fr: string; }[]) => {
-        const initialValue = {};
-        return array.reduce((obj, item) => {
-            return {
-                ...obj,
-                [item.name]: false,
-            };
-        }, initialValue);
-    };
+export default function NewCheckboxArea({gunData, setGunData}: Props){
 
     const { language } = usePreferenceStore()
-    const [checked, setChecked] = useState<{key:boolean} | "">(gunData !== null && gunData.id !== undefined && gunData.id !== "" && gunData[data] !== undefined ? gunData[data] : convertArrayToObject(checkBoxes));
-
-    
 
     function handleCheckBoxCheck(checkBox:string){
 
-        /*
-            status object should be:
-            { checkboxName: boolean, checboxName: boolean etc}
-        */
-console.log(checkBox)
-let newChecked
-if(checked === ""){
-    newChecked = {checkBox: false} // this is for legacy compatibility and broken CSV
-} else {
-    newChecked = checked
-}
-
-        newChecked[checkBox] = !newChecked[checkBox]
-        setChecked(newChecked)
-        setGunData({...gunData, [data]: newChecked})
+        setGunData({...gunData, [checkBox]: !gunData[checkBox]})
     }
 
    
@@ -53,7 +26,7 @@ if(checked === ""){
         <View>
             {checkBoxes.map(checkBox=>{
                 return(
-                    <Checkbox.Item mode={"android"} key={checkBox.name} label={checkBox[language]} status={checked[checkBox.name] ? "checked" : "unchecked"} onPress={()=>{handleCheckBoxCheck(checkBox.name)}}/>
+                    <Checkbox.Item mode={"android"} key={checkBox.name} label={checkBox[language]} status={gunData[checkBox.name] ? "checked" : "unchecked"} onPress={()=>{handleCheckBoxCheck(checkBox.name)}}/>
                 )
             })}
             
