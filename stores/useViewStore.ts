9e138387ea26baf41434267cf8cd96d8f3f85a6a
m00gendai <mrweber@gmx.ch>
2024-10-23 05:25:18 +0200

    Changed for new collections

--- stores/useViewStore.ts
+++ stores/useViewStore.ts
@@ -1,20 +1,9 @@
 import { create } from "zustand"
+import { CollectionItems, ItemTypes, ScreenNames } from "../interfaces"
 
 interface ViewStore {
     mainMenuOpen: boolean
     setMainMenuOpen: () => void
-    newGunOpen: boolean
-    setNewGunOpen: () => void
-    seeGunOpen: boolean
-    setSeeGunOpen: () => void
-    editGunOpen: boolean
-    setEditGunOpen: () => void
-    newAmmoOpen: boolean
-    setNewAmmoOpen: () => void
-    seeAmmoOpen: boolean
-    setSeeAmmoOpen: () => void
-    editAmmoOpen: boolean
-    setEditAmmoOpen: () => void
     lightBoxOpen: boolean
     setLightBoxOpen: () => void
     toastVisible: boolean
@@ -33,24 +22,19 @@ interface ViewStore {
     toggleAmmoSearchVisible: () => void
     searchQueryAmmoCollection: string
     setSearchQueryAmmoCollection: (data: string) => void
-
+    hideBottomSheet: boolean
+    toggleHideBottomSheet: () => void
+    setHideBottomSheet: (status: boolean) => void
+    currentCollectionScreen: ScreenNames
+    setCurrentCollectionScreen: (screen: ScreenNames) => void
   }
 
   export const useViewStore = create<ViewStore>((set) => ({
     mainMenuOpen: false,
     setMainMenuOpen: () => set((state) => ({ mainMenuOpen: !state.mainMenuOpen })),
-    newGunOpen: false,
-    setNewGunOpen: () => set((state) => ({newGunOpen: !state.newGunOpen})),
-    seeGunOpen: false,
-    setSeeGunOpen: () => set((state) => ({seeGunOpen: !state.seeGunOpen})),
-    editGunOpen: false,
-    setEditGunOpen: () => set((state) => ({editGunOpen: !state.editGunOpen})),
-    newAmmoOpen: false,
-    setNewAmmoOpen: () => set((state) => ({newAmmoOpen: !state.newAmmoOpen})),
-    seeAmmoOpen: false,
-    setSeeAmmoOpen: () => set((state) => ({seeAmmoOpen: !state.seeAmmoOpen})),
-    editAmmoOpen: false,
-    setEditAmmoOpen: () => set((state) => ({editAmmoOpen: !state.editAmmoOpen})),
+    hideBottomSheet: false,
+    toggleHideBottomSheet: () => set((state) => ({hideBottomSheet: !state.hideBottomSheet})),
+    setHideBottomSheet: (status: boolean) => set((state) => ({hideBottomSheet: status})),
     lightBoxOpen: false,
     setLightBoxOpen: () => set((state) => ({lightBoxOpen: !state.lightBoxOpen})),
     toastVisible: false,
@@ -68,5 +52,7 @@ interface ViewStore {
     ammoSearchVisible: false,
     toggleAmmoSearchVisible: () => set((state) => ({ammoSearchVisible: !state.ammoSearchVisible})),
     searchQueryAmmoCollection: "",
-    setSearchQueryAmmoCollection: (data: string) => set((state) => ({searchQueryAmmoCollection: data}))
+    setSearchQueryAmmoCollection: (data: string) => set((state) => ({searchQueryAmmoCollection: data})),
+    currentCollectionScreen: "GunCollection",
+    setCurrentCollectionScreen: (screen: ScreenNames) => set((state) => ({currentCollectionScreen: screen}))
   }))
