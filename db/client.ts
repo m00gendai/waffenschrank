977fa916c3c1b0e0b3379cb0f279b73b0759df3f
m00gendai <mrweber@gmx.ch>
2024-10-26 20:12:19 +0200

    Iterate test db

--- db/client.ts
+++ db/client.ts
@@ -2,5 +2,5 @@ import { drizzle } from "drizzle-orm/expo-sqlite"
 import * as SQLite from "expo-sqlite"
 import { openDatabaseSync } from "expo-sqlite/next"
 
-export const expo = openDatabaseSync("test_db37.db", {enableChangeListener: true})
+export const expo = openDatabaseSync("test_db38.db", {enableChangeListener: true})
 export const db = drizzle(expo)
